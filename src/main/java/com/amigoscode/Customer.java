6ba7542b722a2d5d55ebb511c117a2ca0766c158
ikechukwu <gospelihueze@gmail>
2023-06-14 17:17:46 +0100

    Exclude application.properties

---
+++ src/main/java/com/amigoscode/Customer.java
@@ -0,0 +1,28 @@
+package com.amigoscode;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+}
